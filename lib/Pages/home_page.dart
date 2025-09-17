5b2f982f5630049353692eb3169d0dcb3ad90b0b
Douglas <doug.balbino@hotmail.com>
2025-09-17 20:05:19 -0300

    adicionado o icone e o titulo da homepage

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,11 +1,8 @@
- // This widget is the root of your application.
-  import 'package:flutter/material.dart';
-
-
+// This widget is the root of your application.
+import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
@@ -14,43 +11,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.flutter_dash, size: 100, color: Colors.orange),
+                RichText(
+                  text: TextSpan(
+                    // Estilo base para todo o texto, que será herdado
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    children: <TextSpan>[
+                      // O primeiro TextSpan para a palavra "Diego" com a cor laranja
+                      TextSpan(
+                        text: 'Diego ',
+                        style: TextStyle(color: Colors.orange),
+                      ),
+                      // O segundo TextSpan para a palavra "Aquila" com a cor azul
+                      TextSpan(
+                        text: 'Aquila',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
