6c5d910c0ac8e20b88e1a554b8ad82ad6fe05676
Douglas <doug.balbino@hotmail.com>
2025-09-19 22:05:12 -0300

    iniciei a pagina de perfil

--- lib/Pages/profile.dart
+++ lib/Pages/profile.dart
@@ -52,95 +52,27 @@ class _MyProfileState extends State<MyProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.propane_outlined, size: 100, color: Colors.orange),
+      body: Padding(
+        padding: EdgeInsetsGeometry.only(
+          left: 20,
+          top: 20,
+          right: 20,
+          bottom: 20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
 
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-
-                  children: [
-                    Center(
-                      child: Text(
-                        'Diego Áquila',
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.orange,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-
-                    SizedBox(height: 10),
-
-                    Center(
-                      child: Text(
-                        'Full-stack Mobile Developer',
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.orange,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: Text(
-                        'Building beautiful and functional apps.',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-                const SizedBox(height: 50),
-
-                SizedBox(
-                  width: 320,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      labelText: 'Email',
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                SizedBox(
-                  width: 320,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      labelText: 'Password',
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 40.0, top: 10.0),
-                    child: Text(
-                      'Forgot Password?',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ),
-
-                const SizedBox(height: 50),
-              ],
+          children: [
+            Container(
+              width: 200,
+              decoration: BoxDecoration(
+                border: Border.all(width: 8),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Center(child: Icon(Icons.person, size: 200)),
             ),
-          ),
+          ],
         ),
       ),
 
