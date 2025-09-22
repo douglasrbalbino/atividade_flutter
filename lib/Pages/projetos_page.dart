1b032859769841bd926f29e892a1c996e72dc1c6
Douglas <doug.balbino@hotmail.com>
2025-09-22 20:56:28 -0300

    transformei os cards em um componente reutilizavel dentro da pasta widgets

--- lib/Pages/projetos_page.dart
+++ lib/Pages/projetos_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_application_2/Config/app_colors.dart';
 import 'package:flutter_application_2/Config/app_text_style.dart';
 import 'package:flutter_application_2/Pages/home_page.dart';
 import 'package:flutter_application_2/Pages/profile.dart';
+import 'package:flutter_application_2/Widgets/project_cards.dart';
 
 class ProjetosPage extends StatefulWidget {
   const ProjetosPage({super.key});
@@ -64,230 +65,35 @@ class _ProjetosPageState extends State<ProjetosPage> {
           children: [
             Text("Gerenciar projetos", style: AppTextStyle.subtitlePage),
 
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 20,
-                ),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.folder_copy,
-                            size: 30,
-                            color: Colors.orange,
-                          ),
-                          SizedBox(width: 16),
-                          Text(
-                            "App de Notas",
-                            style: AppTextStyle.titleAppBar.copyWith(
-                              fontSize: 25,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 45,
-                        right: 30,
-                        top: 10,
-                      ),
-                      child: Text(
-                        "Aplicativo para gerenciamento de notas dos alunos do Senac.",
-                        style: AppTextStyle.subtitlePage.copyWith(fontSize: 16),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        SizedBox(
-                          width: 100,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                              shape: WidgetStatePropertyAll(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadiusGeometry.circular(
-                                    8,
-                                  ),
-                                ),
-                              ),
-                              backgroundColor: WidgetStatePropertyAll(
-                                Colors.orange,
-                              ),
-                            ),
-                            child: Text(
-                              "Acessar",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+            ProjectCards(
+              iconeCard: Icons.folder,
+              tituloCard: "App de Notas",
+              descricaoCard: "Gerenciamento de notas dos alunos.",
+              tituloBotao: "Acessar",
+              funcao: () {},
             ),
 
-            SizedBox(height: 12),
+            SizedBox(height: 10),
 
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 20,
-                ),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.folder_copy,
-                            size: 30,
-                            color: Colors.orange,
-                          ),
-                          SizedBox(width: 16),
-                          Text(
-                            "API de ChatBot",
-                            style: AppTextStyle.titleAppBar.copyWith(
-                              fontSize: 25,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 45,
-                        right: 30,
-                        top: 10,
-                      ),
-                      child: Text(
-                        "Aplicativo para gerenciamento de notas dos alunos do Senac.",
-                        style: AppTextStyle.subtitlePage.copyWith(fontSize: 16),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        SizedBox(
-                          width: 100,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                              shape: WidgetStatePropertyAll(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadiusGeometry.circular(
-                                    8,
-                                  ),
-                                ),
-                              ),
-                              backgroundColor: WidgetStatePropertyAll(
-                                Colors.orange,
-                              ),
-                            ),
-                            child: Text(
-                              "Acessar",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+            ProjectCards(
+              iconeCard: Icons.folder,
+              tituloCard: "API de ChatBot",
+              descricaoCard: "API desenvolvida para uso do chatbot.",
+              tituloBotao: "Acessar",
+              funcao: () {},
             ),
 
-            SizedBox(height: 12),
+            SizedBox(height: 10),
 
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 20,
-                ),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.folder_copy,
-                            size: 30,
-                            color: Colors.orange,
-                          ),
-                          SizedBox(width: 16),
-                          Text(
-                            "App de Calculadora",
-                            style: AppTextStyle.titleAppBar.copyWith(
-                              fontSize: 25,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 45,
-                        right: 30,
-                        top: 10,
-                      ),
-                      child: Text(
-                        "Aplicativo para gerenciamento de notas dos alunos do Senac.",
-                        style: AppTextStyle.subtitlePage.copyWith(fontSize: 16),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        SizedBox(
-                          width: 100,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                              shape: WidgetStatePropertyAll(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadiusGeometry.circular(
-                                    8,
-                                  ),
-                                ),
-                              ),
-                              backgroundColor: WidgetStatePropertyAll(
-                                Colors.orange,
-                              ),
-                            ),
-                            child: Text(
-                              "Acessar",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+            ProjectCards(
+              iconeCard: Icons.sports_soccer_rounded,
+              tituloCard: "Ronaldo",
+              descricaoCard: "Você é o Ronaldo?",
+              tituloBotao: "Certeza?",
+              funcao: () {},
             ),
 
-            SizedBox(height: 12),
+            SizedBox(height: 10),
           ],
         ),
       ),
---
+++ lib/Widgets/project_cards.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/Config/app_text_style.dart';
+
+class ProjectCards extends StatelessWidget {
+  final IconData iconeCard;
+  final String tituloCard;
+  final String descricaoCard;
+  final String tituloBotao;
+  final VoidCallback funcao;
+
+  const ProjectCards({
+    super.key,
+    required this.iconeCard,
+    required this.tituloCard,
+    required this.descricaoCard,
+    required this.tituloBotao,
+    required this.funcao,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Row(
+                children: [
+                  Icon(iconeCard, size: 30, color: Colors.orange),
+                  SizedBox(width: 16),
+                  Text(
+                    tituloCard,
+                    style: AppTextStyle.titleAppBar.copyWith(fontSize: 25),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 45, right: 30, top: 10),
+              child: Text(
+                descricaoCard,
+                style: AppTextStyle.subtitlePage.copyWith(fontSize: 16),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: 110,
+                  child: ElevatedButton(
+                    onPressed: funcao,
+                    style: ButtonStyle(
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(8),
+                        ),
+                      ),
+                      backgroundColor: WidgetStatePropertyAll(Colors.orange),
+                    ),
+                    child: Text(
+                      tituloBotao,
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
